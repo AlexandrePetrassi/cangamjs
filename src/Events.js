4ec4728618a75a18e0169b4bd4240141e0990fc1
AlexandrePetrassi <alexandrepetrassi@gmail.com>
2019-06-27 10:53:36 -0300

    Added Layered Event class to Events namespace

--- src/Events.js
+++ src/Events.js
@@ -11,7 +11,7 @@
   //---------------------------------------------------------------------------
   //  Prototype class which manages simple event objects
   //===========================================================================
-  self.Event = class Event {
+  self.Event = class {
     //-------------------------------------------------------------------------
     // private fields declaration
     //-------------------------------------------------------------------------
@@ -61,6 +61,73 @@
     //-------------------------------------------------------------------------
     invoke(eventData = {}) {
       this.#callbacks.forEach(callback => callback(eventData));
+    }
+  }
+  //===========================================================================
+  //  ** LayeredEvent
+  //---------------------------------------------------------------------------
+  //  Prototype class which manages multiple events as an index list of events
+  //===========================================================================
+  self.LayeredEvent = class {
+    //-------------------------------------------------------------------------
+    // private fields declaration
+    //-------------------------------------------------------------------------
+    #layers;                 // List of events. Each Event is called a layer.
+    //-------------------------------------------------------------------------
+    //  * Object Initialization
+    //-------------------------------------------------------------------------
+    constructor() {
+      this.#layers = [];
+    }
+    //-------------------------------------------------------------------------
+    //  * Returns the event layer of index 'layer'
+    //-------------------------------------------------------------------------
+    #layer = (layer) =>
+      this.#layers[layer] || (this.#layers[layer] = new self.Event());
+    //-------------------------------------------------------------------------
+    //  * Adds a callback to a layer's end.
+    //
+    //    callback : The callback function which will be added to the layer.
+    //    layer    : The layer's index.
+    //-------------------------------------------------------------------------
+    addEventListener(callback, layer = 0) {
+      this.#layer(layer).addEventListener(callback);
+    }
+    //-------------------------------------------------------------------------
+    //  * Removes a callback from a layer
+    //
+    //    callback : The callback function which will be removed from the layer
+    //    layer    : The layer's index.
+    //-------------------------------------------------------------------------
+    removeEventListener(callback, layer = 0) {
+      this.#layer(layer).removeEventListener(callback);
+    }
+    //-------------------------------------------------------------------------
+    //  * Alias to addEventListener
+    //
+    //    callback : The callback function which will be added to the layer.
+    //    layer    : The layer's index.
+    //-------------------------------------------------------------------------
+    add(callback, layer = 0) {
+      this.addEventListener(callback, layer);
+    }
+    //-------------------------------------------------------------------------
+    //  * Alias to removeEventListener
+    //
+    //    callback : The callback function which will be removed from the layer
+    //    layer    : The layer's index.
+    //-------------------------------------------------------------------------
+    remove(callback, layer = 0) {
+      this.removeEventListener(callback, layer);
+    }
+    //-------------------------------------------------------------------------
+    //  * Invokes every callback function listening to this layered event in
+    //  each layer in order.
+    //
+    //    eventData : Optional event data to be passed as argument.
+    //-------------------------------------------------------------------------
+    invoke(eventData = {}) {
+      this.#layers.forEach(layer => layer ? layer.invoke(eventData) : layer)
     }
   }
 }()
