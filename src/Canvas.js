2cb8daf78946e0f068b7e34728dc583b249af89f
AlexandrePetrassi <alexandrepetrassi@gmail.com>
2019-07-25 21:05:14 -0300

    Transfered IIFE's content inside a conteined class

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,8 +1,3 @@
-//=============================================================================
-//  ► Canvas
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//  Initializes and Manages the Canvas inside the CanGam instance.
-//=============================================================================
 {
   //---------------------------------------------------------------------------
   // imports
@@ -10,35 +5,38 @@
   const self   = document.currentScript.self;
   const cangam = self.cangam;
   const Config = cangam.Config;
-  //---------------------------------------------------------------------------
-  //  private fields declaration
-  //---------------------------------------------------------------------------
-  let targetID;              // id attribute for the div to receive the canvas
-  let parentNode;            // div which will receive the canvas
-  let canvas;                // canvas element that will be inserted in the div
-  //---------------------------------------------------------------------------
-  //  * Initializes the private fields
-  //---------------------------------------------------------------------------
-  const initialize = () => {
-    targetID   = cangam.attributes.target_id;
-    parentNode = document.getElementById(targetID);
-    canvas     = createCanvas(Config.canvas.style);
-    parentNode.appendChild(canvas);
+  //===========================================================================
+  //  ► Canvas
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  //  Initializes and Manages the Canvas inside the CanGam instance.
+  //===========================================================================
+  self.Canvas = new class {
+    //-------------------------------------------------------------------------
+    //  private fields declaration
+    //-------------------------------------------------------------------------
+    targetID;                // id attribute for the div to receive the canvas
+    parentNode;              // div which will receive the canvas
+    canvas;                  // canvas element that will be inserted in the div
+    //-------------------------------------------------------------------------
+    //  * Initializes the private fields
+    //-------------------------------------------------------------------------
+    constructor() {
+      this.targetID   = cangam.attributes.target_id;
+      this.parentNode = document.getElementById(this.targetID);
+      this.canvas     = this.createCanvas(Config.canvas.style);
+      this.parentNode.appendChild(this.canvas);
+    }
+    //-------------------------------------------------------------------------
+    //  * Creates a stylized Canvas element using the Config file data.
+    //-------------------------------------------------------------------------
+    createCanvas(style = {}) {
+      let canvasElement = document.createElement('canvas');
+      let styles        = Object.entries(style);
+      let styleMapping  = (key, value) => canvasElement.style[key] = value;
+      styles.map(pair => styleMapping(...pair));
+      return canvasElement;
+    }
   }
-  //---------------------------------------------------------------------------
-  //  * Creates a stylized Canvas element using the Config file data.
-  //---------------------------------------------------------------------------
-  const createCanvas = (style = {}) => {
-    let canvasElement = document.createElement('canvas');
-    let styles        = Object.entries(style);
-    let styleMapping  = (key, value) => canvasElement.style[key] = value;
-    styles.map(pair => styleMapping(...pair));
-    return canvasElement;
-  }
-  //---------------------------------------------------------------------------
-  //  * Automatically starts the script, such as invoking the initialize method
-  //---------------------------------------------------------------------------
-  { initialize() }
 }
 //=============================================================================
 // END OF SCRIPT
--- src/Main.js
+++ src/Main.js
@@ -1,40 +1,38 @@
-//=============================================================================
-//  ► Main
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//  Initializes and Manages the Main Loop of this CanGam instance.
-//=============================================================================
 {
   //---------------------------------------------------------------------------
   // imports
   //---------------------------------------------------------------------------
   const self   = document.currentScript.self;
   const events = self.cangam.events;
-  //---------------------------------------------------------------------------
-  // private fields declaration
-  //---------------------------------------------------------------------------
-  let baseFrameRate;         // Main loop refresh rate
-  let onUpdate;              // Update event called every frame
-  let onDraw;                // Draw event called every frame
-  //---------------------------------------------------------------------------
-  //  * Initializes the private fields
-  //---------------------------------------------------------------------------
-  const initialize = () => {
-    baseFrameRate = 1/60;
-    onUpdate      = new events.Event;
-    onDraw        = new events.LayeredEvent;
-    window.setInterval(mainLoop, baseFrameRate);
+  //===========================================================================
+  //  ► Main
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  //  Initializes and Manages the Main Loop of this CanGam instance.
+  //===========================================================================
+  self.Main = new class {
+    //-------------------------------------------------------------------------
+    // private fields declaration
+    //-------------------------------------------------------------------------
+    baseFrameRate;         // Main loop refresh rate
+    onUpdate;              // Update event called every frame
+    onDraw;                // Draw event called every frame
+    //-------------------------------------------------------------------------
+    //  * Initializes the private fields
+    //-------------------------------------------------------------------------
+    constructor() {
+      this.baseFrameRate = 1/60;
+      this.onUpdate      = new events.Event;
+      this.onDraw        = new events.LayeredEvent;
+      window.setInterval(() => this.mainLoop(), this.baseFrameRate);
+    }
+    //-------------------------------------------------------------------------
+    //  * Executes the main events at every frame
+    //-------------------------------------------------------------------------
+    mainLoop() {
+      this.onUpdate.invoke();
+      this.onDraw  .invoke();
+    }
   }
-  //---------------------------------------------------------------------------
-  //  * Executes the main events at every frame
-  //---------------------------------------------------------------------------
-  const mainLoop = () => {
-    onUpdate.invoke();
-    onDraw  .invoke();
-  }
-  //---------------------------------------------------------------------------
-  //  * Automatically starts the script, such as invoking the initialize method
-  //---------------------------------------------------------------------------
-  { initialize() }
 }
 //=============================================================================
 // END OF SCRIPT
